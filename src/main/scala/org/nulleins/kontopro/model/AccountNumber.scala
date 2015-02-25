ef1f667a11a4c409da07a14987f77bab182986dc
sothach <phillips.roy@gmail.com>
2015-02-25 13:27:15 +0000

    Update AccountNumber.scala

--- src/main/scala/org/nulleins/kontopro/model/AccountNumber.scala
+++ src/main/scala/org/nulleins/kontopro/model/AccountNumber.scala
@@ -1,12 +1,10 @@
 package org.nulleins.kontopro.model
-
 import scalaz._
 
 trait AccountNumber {
-  val stars = "******************************"
   val noise = """[\s\.,_/:-]""".r
 
-  /** @return an Option of the supplied `code` with non-significant characters removed */
+  /** @return a Validation of the supplied `code` with non-significant characters removed */
   def normalize(code: String, min: Int): Validation[String,String] = {
     if(code == null) {
       Failure("Code may not be null")
@@ -16,16 +14,19 @@ trait AccountNumber {
     }
   }
 
-  /** @return a version of the supplied account number, with all but
-    *         the specified initial and final portion replaced by stars
+  private val stars: Int => String = Memo.mutableHashMapMemo[Int, String] { num =>
+    (for(_ <- 1 to num) yield "*").mkString
+  }
+
+  /** @return a copy of the supplied account number, with all but the
+    *         specified initial and final portion beclouded by stars
     * @param value of the account number
-    * @param initial length to be displayed in clear
-    * @param end length to be displayed in clear */
-  def obsusticate( value: String, initial: Int, end: Int) = {
-    val length = value.length
-    val newInitial = if(initial < length) initial else 1
-    val newEnd = if(end < length) end else 1
-    val oblen = length - newInitial - newEnd
-    s"${value.substring(0,newInitial)}${stars.substring(0,math.min(oblen,length))}${value.substring(length - end)}"
+    * @param first length to be displayed in clear
+    * @param last length to be displayed in clear */
+  def obfuscate( value: String, first: Int, last: Int) = {
+    val prefix = if(first >= 0 && first < value.length) first else 1
+    val suffix = if(last >= 0 && last < value.length) last else 1
+    val obstars = stars(math.min(value.length - prefix - suffix,value.length))
+    s"${value take prefix}$obstars${value drop value.length-suffix}"
   }
 }
